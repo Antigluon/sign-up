c1e32410baefbd025552035a6ba9b6fe995f4711
antigluon <optionaloctave@gmail.com>
2015-08-12 22:53:42 +0000

    Placeholder Template & View

--- events/views.py
+++ events/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import *
 from django.http import HttpResponse
 from django.core.urlresolvers import reverse
 from django.views import generic
@@ -6,4 +6,5 @@ from django.utils import timezone
 
 # Create your views here.
 def index(request):
-    return HttpResponse("You're looking at question?")
+    context = {"title" : "Events - Home", "content" : "placeholder"}
+    return render(request, 'base.html', context)
--- sign_up/settings.py
+++ sign_up/settings.py
@@ -77,8 +77,10 @@ USE_L10N = True
 
 USE_TZ = True
 
+TEMPLATE_DIRS = ["sign_up/templates"]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
