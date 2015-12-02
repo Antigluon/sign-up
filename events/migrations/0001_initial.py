40ef63207938902781a0093c090f90e692e86645
antigluon <optionaloctave@gmail.com>
2015-12-02 20:51:54 +0000

    Many Minor Changes

--- events/migrations/0001_initial.py
+++ events/migrations/0001_initial.py
@@ -13,14 +13,41 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
+            name='Comment',
+            fields=[
+                ('id', models.AutoField(serialize=False, verbose_name='ID', auto_created=True, primary_key=True)),
+                ('text', models.CharField(max_length=8000)),
+            ],
+        ),
+        migrations.CreateModel(
             name='Event',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.AutoField(serialize=False, verbose_name='ID', auto_created=True, primary_key=True)),
                 ('date', models.DateTimeField(verbose_name='Date of Event')),
                 ('place', models.CharField(max_length=1000)),
                 ('name', models.CharField(max_length=255)),
+                ('desc', models.CharField(max_length=10000, null=True)),
                 ('pub_date', models.DateTimeField(verbose_name='Date when people can sign up')),
-                ('signed_up', models.ManyToManyField(to=settings.AUTH_USER_MODEL)),
+                ('signed_up', models.ManyToManyField(blank=True, to=settings.AUTH_USER_MODEL)),
             ],
+        ),
+        migrations.CreateModel(
+            name='Image',
+            fields=[
+                ('id', models.AutoField(serialize=False, verbose_name='ID', auto_created=True, primary_key=True)),
+                ('image', models.ImageField(upload_to='events/images/')),
+                ('caption', models.CharField(blank=True, max_length=2000)),
+                ('event', models.ForeignKey(to='events.Event')),
+            ],
+        ),
+        migrations.AddField(
+            model_name='comment',
+            name='event',
+            field=models.ForeignKey(to='events.Event'),
+        ),
+        migrations.AddField(
+            model_name='comment',
+            name='user',
+            field=models.ForeignKey(to=settings.AUTH_USER_MODEL),
         ),
     ]
