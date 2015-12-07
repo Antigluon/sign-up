e3beb35b4894eed1194bde8a7b4ee07f8d915075
antigluon <optionaloctave@gmail.com>
2015-12-07 23:09:41 +0000

    Setups

--- sign_up/settings.py
+++ sign_up/settings.py
@@ -128,6 +128,26 @@ TEMPLATE_DIRS = ["sign_up/templates"]
 
 STATIC_URL = '/static/'
 
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 MEDIA_URL = "/media/"
