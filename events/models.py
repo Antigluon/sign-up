50f149dbb3bd1b8bb49c800f50965c9758b46b7a
antigluon <optionaloctave@gmail.com>
2016-01-18 22:49:46 +0000

    More working code.

--- events/models.py
+++ events/models.py
@@ -1,15 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.timezone import now
 
 class Event(models.Model):
-    date = models.DateTimeField("Date of Event")
+    date = models.DateField("Date of Event")
+    time = models.CharField(max_length = 1000)
     place = models.CharField(max_length = 1000)
     name = models.CharField(max_length = 255)
-    desc = models.CharField(max_length = 10000, null = True)
+    desc = models.CharField("Description", max_length = 10000, null = True)
     pub_date = models.DateTimeField("Date when people can sign up")
     signed_up = models.ManyToManyField(User, blank = True)
     
-    
+    def viewable(self):
+        return now() >= self.pub_date
+
     def __str__(self):
         return str(self.name)
         
