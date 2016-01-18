1bc42e698d54b355496111f231b479c7d9d8c45e
antigluon <optionaloctave@gmail.com>
2016-01-18 18:56:09 +0000

    Fixed filter button layout issue on mobile, made the attend feature less confusing, and make attending show actual names

--- sign_up/forms.py
+++ sign_up/forms.py
@@ -4,14 +4,18 @@ from django.contrib.auth.forms import UserCreationForm
 
 class UserCreateForm(UserCreationForm):
     email = forms.EmailField(required=True)
+    first_name = forms.CharField(required=True)
+    last_name = forms.CharField(required = True)
 
     class Meta:
         model = User
-        fields = ("username", "email", "password1", "password2")
+        fields = ("username", "email", "password1", "password2", "first_name", "last_name")
 
     def save(self, commit=True):
         user = super(UserCreateForm, self).save(commit=False)
         user.email = self.cleaned_data["email"]
+        user.first_name = self.cleaned_data["first_name"]
+        user.last_name = self.cleaned_data["last_name"]
         if commit:
             user.save()
         return user
