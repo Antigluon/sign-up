928df3d879f759d07b374b27014fcb57282aedad
antigluon <optionaloctave@gmail.com>
2016-03-13 22:41:07 +0000

    Fixing stuf

--- events/models.py
+++ events/models.py
@@ -8,7 +8,7 @@ class Event(models.Model):
     place = models.CharField(max_length = 1000)
     name = models.CharField(max_length = 255)
     desc = models.CharField("Description", max_length = 10000, null = True)
-    pub_date = models.DateTimeField("does not work.", null=True, blank=True)
+    pub_date = models.DateTimeField("does not work.", blank=True)
     signed_up = models.ManyToManyField(User, blank = True)
     
     def viewable(self):
